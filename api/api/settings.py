fd0a8ac8e288db36cf70566270362abfec3f7d59
nonwander <nonwander@gmail.com>
2022-01-17 16:09:20 +0300

    Создал модель участников (клиентов)
    Создано и зарегистрировано в settings.py приложение clients.
    Создана абстрактная модель участника.
    Установлен пакет для работы с изображениями Piilow.
    Добавлен путь в settings.py к хранилищу изображений участников.

--- api/api/settings.py
+++ api/api/settings.py
@@ -30,6 +30,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'debug_toolbar',
     'rest_framework',
+    'sorl.thumbnail',
+    'clients',
 ]
 
 MIDDLEWARE = [
@@ -113,3 +115,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
---
+++ api/clients/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class Client(AbstractUser):
+    CLIENT_GENDER = (
+        ('М', 'Мужчина'),
+        ('Ж', 'Женщина')
+    )
+    avatar = models.ImageField(upload_to="clients/", blank=False, null=True)
+    gender = models.CharField(max_length=1, choices=CLIENT_GENDER)
+    username = models.CharField('client_name', max_length=150, unique=True)
+    first_name = models.TextField('first_name', max_length=30)
+    last_name = models.TextField('last_name', max_length=150)
+    password = models.TextField('password', max_length=150)
+    email = models.EmailField('e-mail', max_length=254, unique=True)
+
+    def __str__(self):
+        return self.username
